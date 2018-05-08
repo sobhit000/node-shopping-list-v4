291d4a183996aaae51a7746b6daee03b67bf7145
Sobhit Sharma <sobhitsharma@hotmail.com>
2018-05-07 22:31:21 -0500

    put recipe

--- server.js
+++ server.js
@@ -62,18 +62,19 @@ app.put('/shopping-list/:id', jsonParser, (req, res) => {
       return res.status(400).send(message);
     }
   }
-
   if (req.params.id !== req.body.id) {
     const message = `Request path id (${req.params.id}) and request body id (${req.body.id}) must match`;
     console.error(message);
     return res.status(400).send(message);
   }
   console.log(`Updating shopping list item \`${req.params.id}\``);
+
   ShoppingList.update({
     id: req.params.id,
     name: req.body.name,
     budget: req.body.budget
   });
+
   res.status(204).end();
 });
 
@@ -105,9 +106,39 @@ app.post('/recipes', jsonParser, (req, res) => {
   res.status(201).json(item);
 });
 
+// when PUT request comes in with updated recipe, ensure has
+// required fields. also ensure that recipe id in url path, and
+// recipe id in updated item object match. if problems with any
+// of that, log error and send back status code 400. otherwise
+// call `Recipes.updateItem` with updated recipe.
+app.put('/recipes/:id', jsonParser, (req, res) => {
+  const requiredFields = ['name', 'ingredients', 'id'];
+  for (let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+  if (req.params.id !== req.body.id) {
+    const message = `Request path id (${req.params.id}) and request body id (${req.body.id}) must match`;
+    console.error(message);
+    return res.status(400).send(message);
+  }
+  console.log(`Updating recipe \`${req.params.id}\``);
+  Recipes.update({
+    id: req.params.id,
+    name: req.body.name,
+    ingredients: req.body.ingredients
+  });
+  res.status(204).end();
+});
+
+// Delete recipes (by id)!
 app.delete('/recipes/:id', (req, res) => {
   Recipes.delete(req.params.id);
-  console.log(`Deleted recipe \`${req.params.ID}\``);
+  console.log(`Deleted shopping list item \`${req.params.ID}\``);
   res.status(204).end();
 });
 
